b644c013b0560c2acb1e3d769e2bd79735ca1e10
Ian Corral <ian.corr.mar@gmail.com>
2025-08-19 16:37:49 -0600

    Add Onboarding flow with pager

--- AndroidClassMP1/app/src/main/java/com/jorgeromo/androidClassMp1/MainActivity.kt
+++ AndroidClassMP1/app/src/main/java/com/jorgeromo/androidClassMp1/MainActivity.kt
@@ -13,6 +13,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.jorgeromo.androidClassMp1.navigation.TabBarNavigationView
 import com.jorgeromo.androidClassMp1.ui.theme.AndroidClassMP1Theme
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.jorgeromo.androidClassMp1.firstpartial.onboarding.viewmodel.OnboardingViewModel
+import com.jorgeromo.androidClassMp1.firstpartial.onboarding.views.OnboardingView
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +23,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AndroidClassMP1Theme {
-                TabBarNavigationView()
+                val vm: OnboardingViewModel = viewModel()
+                OnboardingView(viewModel = vm)
+                // TabBarNavigationView()
             }
         }
     }
